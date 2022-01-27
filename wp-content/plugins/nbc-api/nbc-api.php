8553c11c635277320a8cd55ac507951e79a5040b
Sean Roberts <sean@seankyleandmanley.com>
2022-01-27 15:43:41 -0500

    adds comments so reader can understand dev approach

--- wp-content/plugins/nbc-api/nbc-api.php
+++ wp-content/plugins/nbc-api/nbc-api.php
@@ -5,43 +5,56 @@
 
 function nbc_api_get_feed_data() {
 
+	// adds posts exists admin function to plugin
 	if ( ! function_exists( 'post_exists' ) ) {
     	require_once( ABSPATH . 'wp-admin/includes/post.php' );
 	}
 
+	// lets get our feed url
 	$feed_url = 'https://www.nbcnewyork.com/?rss=y&most_recent=y';
 
+	// lets move forward if the url is valid
 	if($feed_url):
 
+		// load feed xml
 		$feed_data = simplexml_load_file( $feed_url );
 
+		// convert to json
 		$feed_data_json = json_encode($feed_data);
 
+		// convert json to php array
 		$feed_array = json_decode($feed_data_json,TRUE);
 
+		// get items array from feed array
 		$feed_array_items = $feed_array['channel']['item'];
 
+		// lets grab the first 15 items from the array
 		$feed_array_items_slice = array_slice($feed_array_items, 1, 15, true);
 
+		// var dump area for troubleshooting
 		echo '<pre>';
 		var_dump($feed_array_items_slice);
 		echo '</pre>';
 
+		// move through our array of latest 15 feed items
 		foreach($feed_array_items_slice as $item):
 
+				// checking if a post already exists
 				$found_post = post_exists( $item['title'],'','','');
 
+					// if the post is not found
 					if(!$found_post):
 
-					$post_id = wp_insert_post(
-						array(
-							'post_title' =>	$item['title'],
-							//'post_date' => $item['pubDate'],
-							//'post_content' => $item['description'],
-							'post_status' =>	'publish',
-							'post_type' =>	'post'
-						)
-					);
+						// create the post
+						$post_id = wp_insert_post(
+							array(
+								'post_title' =>	$item['title'],
+								//'post_date' => $item['pubDate'],
+								//'post_content' => $item['description'],
+								'post_status' =>	'publish',
+								'post_type' =>	'post'
+							)
+						);
 
 				endif;
 
@@ -52,5 +65,6 @@ function nbc_api_get_feed_data() {
 	return false;
 }
 
+// run the function each time page with shortcode is loaded for now
 add_shortcode( 'feed', 'nbc_api_get_feed_data' );
 //add_filter( 'after_setup_theme', 'nbc_api_get_feed_data' );
