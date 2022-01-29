25b81dd6dbfae961b098b82e426395c7284ab32d
Sean Roberts <sean@seankyleandmanley.com>
2022-01-29 15:18:13 -0500

    starts building api endpoint

--- wp-content/plugins/nbc-api/nbc-api.php
+++ wp-content/plugins/nbc-api/nbc-api.php
@@ -71,4 +71,28 @@ function nbc_api_create_posts_from_xml_feed_data() {
 }
 
 // run the function each time page with shortcode is loaded for now
-add_shortcode( 'feed', 'nbc_api_create_posts_from_xml_feed_data' );
+//add_shortcode( 'feed', 'nbc_api_create_posts_from_xml_feed_data' );
+
+
+function nbc_api_return_posts() {
+	$args = array(
+		'post_type' => 'post',
+		'posts_per_page' => 10,
+	);
+	$posts = get_posts($args);
+	if (empty($posts)):
+    	return new WP_Error( 'no_posts_found', 'No posts found.', array('status' => 404) );
+    endif;
+    $response = new WP_REST_Response($posts);
+    $response->set_status(200);
+    return $response;
+}
+
+function nbc_api_add_api_route() {
+	register_rest_route( 'testfeed/v1', 'ingestedstories', array(
+                'methods'  => 'GET',
+                'callback' => 'nbc_api_return_posts'
+      ));
+}
+add_action('rest_api_init', 'nbc_api_add_api_route');
+
